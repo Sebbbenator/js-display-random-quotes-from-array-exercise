7596b7fb3a3bc807a943312a7293abd998417e9a
Sebbbenator <eaa25ssg@students.eaaa.dk>
2025-10-30 11:22:19 +0100

    quts

---
+++ js/script.js
@@ -0,0 +1,33 @@
+"use strict"
+
+const quotes = [
+    "Hvorfor være en mand når du kan være en futtemus?",
+    "Martin elev er som en snemand, han smelter ved varmen.",
+    "Livet er som en cykel, for at holde balancen skal du blive ved med at træde i pedalerne.",
+    "Jeg er helt dælens, dog ikke mere end du er.",
+    "livet er som en fisk, nogle gange er det bare en tør sild.",
+    "Mathilde er som en solstråle på en regnvejrsdag.",
+    "NEVER GONNA GIVE YOU UP, NEVER GONNA LET YOU DOWN!",
+]
+
+function getRandomQuote() {
+  if (quotes.length === 0) {
+    return "No quotes available";
+  }
+
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+
+
+  return quotes[randomIndex];
+}
+
+
+function displayRandomQuote() {
+  const randomQuote = getRandomQuote(); // Henter et citat
+  document.getElementById("quotes").textContent = randomQuote; // Viser det på siden
+}
+
+
+document
+  .getElementById("displayQuoteBtn")
+  .addEventListener("click", displayRandomQuote);
